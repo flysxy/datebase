c398f319088564bef438a7f82a70960fd27405e0
emily <sunyingdmn@163.com>
2020-04-13 11:08:49 +0800

    others

---
+++ bTree/BinaryTree.java
@@ -0,0 +1,13 @@
+// binary tree.
+
+// pre search ：preOrder(r) = print r->preOrder(r->left)->preOrder(r->right)
+// mid search: inOrder(r) = inOrder(r->left)->print r->inOrder(r->right)
+// post search：postOrder(r) = postOrder(r->left)->postOrder(r->right)->print r
+
+
+class BinaryTree  {
+
+
+}
+
+// binary search tree;
